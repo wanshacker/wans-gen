5f459468612e1eed791626ffce299b22892db5ee
wanshacker <89900554+wanshacker@users.noreply.github.com>
2021-09-26 18:12:17 +0900

    Gen Source

--- gen.py
+++ gen.py
@@ -1,5 +1,7 @@
 import requests, re
- 
+
+input_count = int(input("Genする数を入力してください>>>"))
+input_count -= 1
  
 def findUrl(string):
     regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
@@ -8,8 +10,10 @@ def findUrl(string):
  
 urls = []
 head = {'gen': ''}
- 
-while True:
+
+
+
+while not input_count < (sum([1 for _ in open('links.txt')])):
     with open("links.txt", "a") as file:
         file.write(findUrl(requests.post('https://masteralts.com/', data=head).content.decode())[8] + "\n")
-    
+
